c0728e3b35a8e0eb1e52cc14a24810a320b1bef4
Quốc Anh <20127001@student.hcmus.edu.vn>
2024-03-14 22:34:20 +0700

    Init ClassRoom Entity and CRUD API

--- quocanh-symfony-crud/src/Service/IClassRoomService.php
+++ quocanh-symfony-crud/src/Service/IClassRoomService.php
@@ -2,7 +2,18 @@
 
 namespace App\Service;
 
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 interface IClassRoomService
 {
+    public function createClassRoom(ManagerRegistry $doctrine, Request $raw, ValidatorInterface $validator): JsonResponse;
+    public function getAllClassRooms(ManagerRegistry $doctrine): JsonResponse;
+    public function getClassById(ManagerRegistry $doctrine, int $id): JsonResponse;
+    public function updateClassInfo(ManagerRegistry $doctrine, int $id, Request $raw, ValidatorInterface $validator): JsonResponse;
+    public function addStudent(ManagerRegistry $doctrine, int $classId, int $studentId): JsonResponse;
+    public function deleteClass(ManagerRegistry $doctrine, int $id): JsonResponse;
 
 }
